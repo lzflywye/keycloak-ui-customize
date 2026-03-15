a6319e1d3c2e54ca3c1c062c233b3796cb8666f6
Aiba Riku <lzflywye@outlook.com>
2026-03-15 11:30:12 +0900

    Build script improvements

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ rm -f $OUTPUT_JAR
 
 echo "Building Keycloak theme JAR: $OUTPUT_JAR ..."
 
-zip -r $OUTPUT_JAR META-INF theme
+jar cvf $OUTPUT_JAR META-INF theme
 
 echo "Done! You can find the JAR at: ./$OUTPUT_JAR"
